e1540164940aad069ef41014de5ae765b7ee6bad
Stancic M <milan.stancic@brightmarbles.io>
2025-01-16 13:12:45 +0100

    use Config.DEFAULT_TIMEOUT on loginpage obj

--- pages/login/LoginPage.py
+++ pages/login/LoginPage.py
@@ -4,48 +4,48 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ExpectedConditions
 from selenium.webdriver.common.action_chains import ActionChains
+from config import Config
 
 class LoginPage:
 
     def __init__(self, driver):
-
         self.driver = driver
-        self.email_input = WebDriverWait(self.driver, 10).until(
+        self.email_input = WebDriverWait(self.driver, Config.DEFAULT_TIMEOUT).until(
             ExpectedConditions.visibility_of_element_located((By.NAME, "email"))
         )
-        self.password_input = WebDriverWait(self.driver, 10).until(
+        self.password_input = WebDriverWait(self.driver, Config.DEFAULT_TIMEOUT).until(
             ExpectedConditions.visibility_of_element_located((By.NAME, "password"))
         )
-        self.submit_btn = WebDriverWait(self.driver, 10).until(
+        self.submit_btn = WebDriverWait(self.driver, Config.DEFAULT_TIMEOUT).until(
             ExpectedConditions.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid='submit_btn']"))
         )
 
     def set_email(self, email):
-
         self.email_input.clear()
         self.email_input.send_keys(email)
 
     def set_password(self, password):
-
         self.password_input.clear()
         self.password_input.send_keys(password)
 
-    def login(self, email, password):
-
+    def login(self, email=None, password=None):
+        # Use default test credentials if none provided
+        email = email or Config.TEST_USER_EMAIL
+        password = password or Config.TEST_USER_PASSWORD
+        
         self.set_email(email)
         self.set_password(password)
         try:
             ActionChains(self.driver).move_to_element(self.submit_btn).click().perform()
-            WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, Config.DEFAULT_TIMEOUT).until(
                 ExpectedConditions.visibility_of_element_located(
                     (By.CSS_SELECTOR, "div[data-testid='profile_card_id']"))
             )
         except TimeoutException:
-            login_page = LoginPage(self.driver)
-            login_page.submit_btn.click()
+            # Instead of recreating the page object, just retry the click
+            self.submit_btn.click()
 
     def check_validation(self, email, password, expected_validation):
-
         self.set_email(email)
         self.set_password(password)
         ActionChains(self.driver).move_to_element(self.submit_btn).click().perform()
@@ -56,18 +56,16 @@ class LoginPage:
             return self.find_email_validation_message(expected_validation)
 
     def find_email_validation_message(self, expected_validation):
-
         # find expected validation
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, Config.DEFAULT_TIMEOUT).until(
             ExpectedConditions.visibility_of_element_located((By.XPATH, "//*[text()='{}']".format(expected_validation)))
         )
         # return true if message refers to email field
         return expected_validation == self.email_input.find_element_by_xpath("./following-sibling::div").text
 
     def find_password_validation_message(self, expected_validation):
-
         # find expected validation
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, Config.DEFAULT_TIMEOUT).until(
             ExpectedConditions.visibility_of_element_located((By.XPATH, "//*[text()='{}']".format(expected_validation)))
         )
         # return true if message refers to password field
